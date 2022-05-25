209689e3e78e8746df4872054c2a132163c49649
hewanab <hewanmulu06@gmail.com>
2022-05-25 23:20:22 +0300

    data cleaning

--- scripts/file.py
+++ scripts/file.py
@@ -2,8 +2,7 @@ import pandas as pd
 import pickle
 import dvc.api
 from time import gmtime, strftime
-from app_logger import App_Logger
-from config import Config
+from logger import App_Logger
 
 class FileHandler():
 
@@ -29,34 +28,12 @@ class FileHandler():
             # self.logger.exception('File not found.')
             pass
 
-    def get_data_url_from_dvc(self, file_path, version):
-        try:
-            data_url = dvc.api.get_url(path=str(file_path), repo=str(Config.REPO), rev=version)
-            # self.logger.info("Data url fetched from dvc.")
-            return df
+#     def get_data_url_from_dvc(self, file_path, version):
+#         try:
+#             data_url = dvc.api.get_url(path=str(file_path), repo=str(Config.REPO), rev=version)
+#             # self.logger.info("Data url fetched from dvc.")
+#             return df
 
-        except Exception:
-            # self.logger.exception("Error while fetching data url from dvc.")
-            pass
-
-    def save_model(self, model, model_name):
-        try:
-            time = strftime("%Y-%m-%d-%H:%M:%S", gmtime())
-            name = Config.MODELS_PATH / str(f'{model_name} {time}.pkl')
-            Config.MODELS_PATH.mkdir(parents=True, exist_ok=True)
-            pickle.dump(model, open(str(name), "wb"))
-            # self.logger.info(f'{model_name} model { model_name + time }.pkl saved in {Config.MODELS_PATH}.')
-
-        except Exception:
-            # self.logger.exception("Model saving failed.")
-            pass
-
-    def read_model(self, model_name):
-        try:
-            name = Config.MODELS_PATH / model_name
-            model = pickle.load(open(str(name), "rb"))
-            # self.logger.info(f'{ model_name }.pkl is read from {Config.MODELS_PATH}.')
-            return model
-        except FileNotFoundError:
-            # self.logger.exception("Model not found.")
-            pass
+#         except Exception:
+#             # self.logger.exception("Error while fetching data url from dvc.")
+#             pass
