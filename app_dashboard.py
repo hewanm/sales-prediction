639b01ae88f4f0ca9bba4d821c265f4bfec07f12
hewanab <hewanmulu06@gmail.com>
2022-06-04 08:45:28 +0300

    streamlit dashboard

---
+++ app_dashboard.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import awesome_streamlit as ast
+import src.pages.home
+import src.pages.data
+import src.pages.pred
+
+ast.core.services.other.set_logging_format()
+
+# create the choices
+PAGES = {
+    "Home": src.pages.home,
+    "Data":src.pages.data,
+    "Run Predictions": src.pages.predict
+}
+
+
+# render the pages
+def main():
+    """Main function of the App"""
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+
+    page = PAGES[selection]
+
+    with st.spinner(f"Loading {selection} ..."):
+        ast.shared.components.write_page(page)
+
+    st.sidebar.title("About")
+    st.sidebar.info(
+        """
+        This App is an end-to-end product that enables the Rossmann pharmaceutical company to 
+        view predictions on sales across their stores.
+"""
+    )
+
+# run it
+if __name__ == "__main__":
+    main()
